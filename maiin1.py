d9b02769255115f313a35ef6d51511d863fd6d2f
PetrDG <devil-ghost@yandex.ru>
2021-11-04 17:18:13 +0300

    Def

---
+++ maiin1.py
@@ -0,0 +1,32 @@
+import os
+
+p1 = '1.txt'
+p2 = '2.txt'
+p3 = '3.txt'
+
+f1 = open('1.txt', 'r', encoding="utf-8")
+s11 = f1.readlines()
+s1 = len(s11)
+f2 = open('2.txt', 'r', encoding="utf-8")
+s22 = f2.readlines()
+s2 = len(s22)
+
+f3 = open('3.txt', 'r', encoding="utf-8")
+s33 = f3.readlines()
+s3 = len(s33)
+
+name = [p1, p2, p3]
+data = [s1, s2, s3]
+valu = [s11, s22, s33]
+result = dict(zip(data, valu))
+
+
+sorted_dict = {}
+sorted_keys = sorted(result)
+for w in sorted_keys:
+    sorted_dict[w] = result[w]
+
+
+with open('Итог.txt', 'w', encoding="utf-8") as file:
+    for key, value in sorted_dict.items():
+        file.write(f'{key}\n, {value}\n')
--- main.py
+++ main.py
@@ -1,58 +1,70 @@
 import os
 
-p1 = '1.txt'
-p2 = '2.txt'
-p3 = '3.txt'
-file = "Итог.txt"
-with open('1.txt', 'r', encoding='utf-8') as f1:
-    file1 = f1.readlines()
-with open('2.txt', 'r', encoding='utf-8') as f2:
-    file2 = f2.readlines()
-with open('3.txt', 'r', encoding='utf-8') as f3:
-    file3 = f3.readlines()
-with open("Итог.txt", 'w', encoding='utf-8') as f:
-    if len(file1) < len(file2) and len(file1) < len(file3):
-        f.write(p1 + '\n')
-        f.write(str(len(file1)) + '\n')
-        f.writelines(file1)
-        f.write('\n')
-    elif len(file2) < len(file1) and len(file2) < len(file3):
-        f.write(p2 + '\n')
-        f.write(str(len(file2)) + '\n')
-        f.writelines(file2)
-        f.write('\n')
-    elif len(file3) < len(file1) and len(file3) < len(file2):
-        f.write(p3 + '\n')
-        f.write(str(len(file3)) + '\n')
-        f.writelines(file3)
-        f.write('\n')
-    if len(file2) > len(file1) > len(file3) or len(file2) < len(file1) < len(file3):
-        f.write(p1 + '\n')
-        f.write(str(len(file1)) + '\n')
-        f.writelines(file1)
-        f.write('\n')
-    elif len(file1) > len(file2) > len(file3) or len(file2) > len(file1) and len(file2) < len(file3):
-        f.write(p2 + '\n')
-        f.write(str(len(file2)) + '\n')
-        f.writelines(file2)
-        f.write('\n')
-    elif len(file1) > len(file3) > len(file2) or len(file3) > len(file1) and len(file3) < len(file2):
-        f.write(p3 + '\n')
-        f.write(str(len(file3)) + '\n')
-        f.writelines(file3)
-        f.write('\n')
-    if len(file1) > len(file2) and len(file1) > len(file3):
-        f.write(p1 + '\n')
-        f.write(str(len(file1)) + '\n')
-        f.writelines(file1)
-    elif len(file2) > len(file1) and len(file2) > len(file3):
-        f.write(p2 + '\n')
-        f.write(str(len(file2)) + '\n')
-        f.writelines(file2)
-    elif len(file3) > len(file1) and len(file3) > len(file2):
-        f.write(p3 + '\n')
-        f.write(str(len(file3)) + '\n')
-        f.writelines(file3)
+def file_rw(p1 = None, p2 = None, p3 = None):
+    if p1 or p2 or p3 is None:
+        p1 = '1.txt'
+        p2 = '2.txt'
+        p3 = '3.txt'
+        file = "Итог.txt"
+        with open('1.txt', 'r', encoding='utf-8') as f1:
+            file1 = f1.readlines()
+        with open('2.txt', 'r', encoding='utf-8') as f2:
+            file2 = f2.readlines()
+        with open('3.txt', 'r', encoding='utf-8') as f3:
+            file3 = f3.readlines()
+        with open("Итог.txt", 'w', encoding='utf-8') as f:
+            if len(file1) < len(file2) and len(file1) < len(file3):
+                f.write(p1 + '\n')
+                f.write(str(len(file1)) + '\n')
+                f.writelines(file1)
+                f.write('\n')
+            elif len(file2) < len(file1) and len(file2) < len(file3):
+                f.write(p2 + '\n')
+                f.write(str(len(file2)) + '\n')
+                f.writelines(file2)
+                f.write('\n')
+            elif len(file3) < len(file1) and len(file3) < len(file2):
+                f.write(p3 + '\n')
+                f.write(str(len(file3)) + '\n')
+                f.writelines(file3)
+                f.write('\n')
+            if len(file2) > len(file1) > len(file3) or len(file2) < len(file1) < len(file3):
+                f.write(p1 + '\n')
+                f.write(str(len(file1)) + '\n')
+                f.writelines(file1)
+                f.write('\n')
+            elif len(file1) > len(file2) > len(file3) or len(file2) > len(file1) and len(file2) < len(file3):
+                f.write(p2 + '\n')
+                f.write(str(len(file2)) + '\n')
+                f.writelines(file2)
+                f.write('\n')
+            elif len(file1) > len(file3) > len(file2) or len(file3) > len(file1) and len(file3) < len(file2):
+                f.write(p3 + '\n')
+                f.write(str(len(file3)) + '\n')
+                f.writelines(file3)
+                f.write('\n')
+            if len(file1) > len(file2) and len(file1) > len(file3):
+                f.write(p1 + '\n')
+                f.write(str(len(file1)) + '\n')
+                f.writelines(file1)
+            elif len(file2) > len(file1) and len(file2) > len(file3):
+                f.write(p2 + '\n')
+                f.write(str(len(file2)) + '\n')
+                f.writelines(file2)
+            elif len(file3) > len(file1) and len(file3) > len(file2):
+                f.write(p3 + '\n')
+                f.write(str(len(file3)) + '\n')
+                f.writelines(file3)
+    return
+
+file_rw()
+
+
+
+
+
+
+
 
 
 
